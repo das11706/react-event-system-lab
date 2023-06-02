ee8b6bdbfd02593afeda7d7af6f202348923d25d
Daniel Suarez <suarez.daniel101@gmail.com>
2023-06-02 13:27:01 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends React.Component {
+
+  updateFocus = () => {
+    console.log('Good!')
+  }
+
+  updateBlur = () => {
+    console.group('Hey! Eyes on me!')
+  }
+
+  render () {
+    return (
+      <button onFocus={this.updateFocus} onBlur={this.updateBlur}></button>
+    )
+  }
+}
+
+export default EyesOnMe;
